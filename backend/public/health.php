82791d7e0b3cb40bf5109c6d7973c90e6a6f902f
cheikhdoss <birakanembodj01@gmail.com>
2025-12-25 06:59:06 +0000

    repaire

---
+++ backend/public/health.php
@@ -0,0 +1,11 @@
+<?php
+// Simple healthcheck endpoint that doesn't require Laravel bootstrap
+header('Content-Type: application/json');
+http_response_code(200);
+echo json_encode([
+    'status' => 'ok',
+    'timestamp' => date('c'),
+    'server' => 'php'
+]);
+exit;
+
